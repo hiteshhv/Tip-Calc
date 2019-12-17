6df6c855ae13e51be30033f14b0ee5caf3b9aa87
hiteshhv <46933160+hiteshhv@users.noreply.github.com>
2019-12-17 14:52:51 +0530

    Push stylesheet and js

---
+++ javascript/javascript.js
@@ -0,0 +1,30 @@
+function calculateTip () {
+ var billAmt = document.getElementById("billamt").value;
+ var serviceQual = document.getElementById("serviceQual").value;
+ var numberOfPeople = document.getElementById("peopleamt").value;
+if (billAmt === "" || serviceQual == 0){
+ alert("Please enter the values");
+ return;
+}
+if (numberOfPeople === "" || numberOfPeople <= 1){
+ numberOfPeople = 1;
+ document.getElementsById("each").style.display = "none";
+}
+else {
+ document.getElementById("each").style.display = "block";
+}
+// calculation
+var total = (billAmt * serviceQual) / numberOfPeople;
+total = Math.round(total * 100) / 100;
+total = total.toFixed(2);
+
+document.getElementById("totaltip").style.display = "block";
+document.getElementById("tip").innerHTML = total;
+}
+//document.getElementById("totaltip").style.display = "none";
+//document.getElementById(each).style.display = "none";
+
+//to call the function
+document.getElementById("calculate").onclick = function () {
+ calculateTip();
+};
